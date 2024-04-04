c02a071448b1754e329a2240f923c46914985892
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-04 15:27:07 +0700

    create navigation

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import {Colors} from '@themes';
+import {hp, wp} from '@utils/StyleUtil';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    paddingTop: hp(3),
+    paddingHorizontal: wp(3),
+  },
+});
+
+export default styles;
