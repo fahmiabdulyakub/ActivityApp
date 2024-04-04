cdc982b34d06b89870c8dbc05238c7334804b4bd
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-04 23:53:09 +0700

    creaate ActivityService

---
+++ src/services/ActivityService.ts
@@ -0,0 +1,18 @@
+import {IActivity} from '@types';
+import axios from 'axios';
+import Config from 'react-native-config';
+
+export const getActivity = (type: string) => {
+  return new Promise<IActivity>(resolve => {
+    axios
+      .get(`${Config.BASE_URL}api/activity`, {
+        params: {type},
+      })
+      .then(result => {
+        resolve(result.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+};
