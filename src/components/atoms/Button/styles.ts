647950bb4b75f09c2031388fb32fb24e13d80e5d
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-04 16:11:18 +0700

    create Button component

---
+++ src/components/atoms/Button/styles.ts
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+import {Colors, Fonts} from '@themes';
+import {hp, wp} from '@utils/StyleUtil';
+
+export default StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    height: hp(5),
+    width: wp(20),
+    borderWidth: 1,
+    borderColor: Colors.light_grey,
+    shadowColor: Colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  title: {
+    fontSize: hp(1.8),
+    color: Colors.white,
+    fontFamily: Fonts.PoppinsMedium,
+    top: 1,
+  },
+});
