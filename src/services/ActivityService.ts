5f7792bcfae5fc40a3b9734bdbd0648bc7cc9c4f
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-05 15:27:42 +0700

    change api

--- src/services/ActivityService.ts
+++ src/services/ActivityService.ts
@@ -2,11 +2,24 @@ import {IActivity} from '@types';
 import axios from 'axios';
 import Config from 'react-native-config';
 
-export const getActivity = (type?: string) => {
+export const getActivity = () => {
   return new Promise<IActivity>(resolve => {
     axios
-      .get(`${Config.BASE_URL}api/activity${!type && '/'}`, {
-        params: type && {type},
+      .get(`${Config.BASE_URL}api/activity/`)
+      .then(result => {
+        resolve(result.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+};
+
+export const getActivityType = (type: string) => {
+  return new Promise<IActivity>(resolve => {
+    axios
+      .get(`${Config.BASE_URL}api/activity`, {
+        params: {type},
       })
       .then(result => {
         resolve(result.data);
