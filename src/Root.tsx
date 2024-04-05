7760bc97e4d28f21d55bb5aaafc1d7e03cfb132b
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-05 15:27:06 +0700

    add gestur handler

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,23 +5,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Colors} from '@themes';
 import {Home} from '@screens';
 import {StackParams} from '@types';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator<StackParams>();
 
 export const Routes = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.SafeAreaView}>
-        <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <SafeAreaView style={styles.SafeAreaView}>
+          <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
