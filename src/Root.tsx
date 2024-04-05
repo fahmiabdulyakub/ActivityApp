bea669ce986a2fdd6eb644a85cda86a9fe307158
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-04-05 16:16:03 +0700

    add History

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,22 +3,28 @@ import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Colors} from '@themes';
-import {Home} from '@screens';
+import {History, Home} from '@screens';
 import {StackParams} from '@types';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {navigationRef} from '@utils/NavigationUtils';
 
 const Stack = createNativeStackNavigator<StackParams>();
 
 export const Routes = () => {
   return (
     <GestureHandlerRootView>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <SafeAreaView style={styles.SafeAreaView}>
           <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen
               name="Home"
               component={Home}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="History"
+              component={History}
               options={{headerShown: false}}
             />
           </Stack.Navigator>
